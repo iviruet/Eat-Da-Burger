771371c5452fd99fa3328ebc64d9699d851bd14e
IV <irisviruet@hotmail.com>
2020-03-05 14:08:14 -0500

    model burger.js

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,12 @@ const connection = require("./connection.js");
 
 const orm = {
     selectAll: function(){
+        const queryString = "SELECT ?? FROM ??";
+        const query = connection.query(queryString, [], (err, result) => {
+            throw err;
+            log("\n" + query.sql);
+            log(result);
+        })
 
     }
     insertOne: function(){
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-insert into burgers (burger_name, devoured) values ("Cheeseburger", "True"),
-insert into burgers (burger_name, devoured) values ("Avocado Burger", "True"),
-insert into burgers (burger_name, devoured) values ("Bacon Burger", "True"),
-insert into burgers (burger_name, devoured) values ("Impossible Burger", "False"),
+insert into burgers (burger_name, devoured) values ("Cheeseburger", True);
+insert into burgers (burger_name, devoured) values ("Avocado Burger", true);
+insert into burgers (burger_name, devoured) values ("Bacon Burger", True);
+insert into burgers (burger_name, devoured) values ("Impossible Burger", False);
